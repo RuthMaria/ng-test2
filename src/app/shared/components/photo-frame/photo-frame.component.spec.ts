79e0d7f073159c728cbd1695a9ffbbc537f864bc
RuthMaria <ruth-15rms@hotmail.com>
2024-05-13 16:12:53 -0300

    adicionado os testes do photo-frame

--- src/app/shared/components/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/components/photo-frame/photo-frame.component.spec.ts
@@ -3,7 +3,7 @@ import { PhotoFrameComponent } from './photo-frame.component';
 import { PhotoFrameModule } from './photo-frame.module';
 
 describe(PhotoFrameComponent.name, () => {
-  let fixture: ComponentFixture<PhotoFrameComponent> = null;
+  let fixture: ComponentFixture<PhotoFrameComponent> = null; // têm uma referência para a representação do template(html) do componente no DOM permitindo que o desenvolvedor possa fazer pesquisas.
   let component: PhotoFrameComponent;
 
   beforeEach(async () => {
@@ -18,4 +18,81 @@ describe(PhotoFrameComponent.name, () => {
   it('Should create component', () => {
     expect(component).toBeTruthy();
   });
+
+  /*
+    Usamos o fakeAsync para fazer o teste esperar/aguardar 500 milissegundos
+    para pode ser executado.
+    A função tick só funciona dentro do escopo da função fakeAsync.
+  */
+  it(`#${PhotoFrameComponent.prototype.like.name}
+    should trigger (@Output liked) once when called
+    multiple times within debounce time`, fakeAsync(() => {
+      fixture.detectChanges();
+      let times = 0;
+      component.liked.subscribe(() => times++);
+      component.like();
+      component.like();
+      tick(500); // espera 500 milissegundos
+      expect(times).toBe(1);
+  }));
+
+  it(`#${PhotoFrameComponent.prototype.like.name}
+    should trigger (@Output liked) two times when
+    called outside debounce time`, fakeAsync(() => {
+      fixture.detectChanges();
+      let times = 0;
+      component.liked.subscribe(() => times++);
+      component.like();
+      tick(500);
+      component.like();
+      tick(500);
+      expect(times).toBe(2);
+    }));
+
+    /*
+    Usamos HTMLElement ao invés de HTMLSpanElement, que seria mais específico porque
+    o elemento que queremos pegar é um span, porque ele é mais genérico e caso quiséssemos
+    trocar o span por outro elemento html o teste não iria quebrar.
+    */
+  it(`(D) Should display number of likes when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+    component.likes++;
+    fixture.detectChanges(); // chamou pela segunda vez para poder atualizar os componentes do DOM com o novo valor
+    const elementLike: HTMLElement = fixture.nativeElement.querySelector('.like-counter');
+    expect(elementLike.textContent.trim()).toBe('1');
+  });
+
+  it(`(D) Should update aria-label when (@Input likes) is incremented`, () => {
+    fixture.detectChanges();
+    component.likes++;
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('1: people liked');
+  });
+
+  it(`(D) Should have aria-label with 0 (@Input likes)`, () => {
+    fixture.detectChanges();
+    const element: HTMLElement = fixture.nativeElement.querySelector('span');
+    expect(element.getAttribute('aria-label')).toBe('0: people liked');
+  });
+
+  it(`(D) Should display image with src and description when bound to properties`, () => {
+    const description = 'some description';
+    const src = 'http://somesite.com/img.jpg';
+    component.src = src;
+    component.description = description;
+    fixture.detectChanges();
+    const img: HTMLImageElement = fixture.nativeElement.querySelector('img');
+    expect(img.getAttribute('src')).toBe(src);
+    expect(img.getAttribute('alt')).toBe(description);
+  });
 });
+
+/*
+Se quisermos saber o tempo de execução de cada teste, para verificarmos quais estão
+demandando bastante tempo de execução, podemos fazer essa verificação através do
+JUnit report (que precisa ser instalado como um plugin do karma).
+
+Basta rodar o script 'npm run test-ci' (que já está configurado com o JUnit) e
+ter acesso a esse relatório.
+*/
